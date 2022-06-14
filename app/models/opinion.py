18e3f743851af1f7f85166d138eff17ef5081d98
Vladyslaw Lakhman <lakhman011@gmail.com>
2022-06-14 19:21:24 +0200

    14.06.2022 YB

--- app/models/opinion.py
+++ app/models/opinion.py
@@ -14,12 +14,21 @@ class Opinion():
         self.pros = pros
         self.cons = cons
         self.opinion_id = opinion_id
-        return self
     
     def extract_opinion(self, opinion):
         for key, value in selectors.items():
             setattr(self, key, get_item(opinion, *value))
         self.opinion_id = opinion["data-entry-id"]
         return self
+
+    def __str__(self) -> str:
+        return f"opinion_id: {self.opinion_id}<br>" + "<br>".join(f"{key}: {str(getattr(self, key))}" for key in selectors.keys())
+
+    def __repr__(self) -> str:
+        return f"Opinion(opinion_id={self.opinion_id}, " + ", ".join(f"{key}={str(getattr(self, key))}" for key in selectors.keys()) + ")"
+
+    def to_dict(self) -> dict:
+        return {"opinion_id": self.opinion_id} | {key: getattr(self, key) for key in selectors.keys()}
+
 
     
